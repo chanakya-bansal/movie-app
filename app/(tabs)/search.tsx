c8b2e9eec273d91d96b33eda4b5667802275ffee
unknown <sweethomemrt@gmail.com>
2025-07-04 03:18:57 +0530

    appwrite setup

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import { icons } from '@/constants/icons';
 import SearchBar from '@/components/SearchBar';
+import { updateSearchCount } from '@/services/appwrite';
 
 const search = () => {
 
@@ -22,9 +23,17 @@ const search = () => {
   }),false)
 
   useEffect(()=>{
+
+
     const timeoutId=setTimeout( async()=>{
       if(searchQuery.trim()){
         await loadMovies();
+
+        if(movies?.length>0 && movies?.[0]){
+          await updateSearchCount(searchQuery,movies[0]);
+        }
+
+
       }else{
         reset();
       }
